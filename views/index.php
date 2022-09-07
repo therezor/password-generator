a03ec1a760e572822a45107e09023991e84acd4c
REZOR <zmrezor@gmail.com>
2022-09-07 21:27:50 +0100

    Add meta description

--- views/index.php
+++ views/index.php
@@ -6,7 +6,7 @@ return <<<ENDOFVIEW
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="A layout example that shows off a responsive product landing page.">
+    <meta name="description" content="Everyone knows they should use strong and unique passwords, but it's tough to memorize them all.  Memorable password generator lets you easily create secure, unique and strong passwords for each site.">
     <title>Password Generator. Strong and memorable passwords.</title>
     <link rel="stylesheet" href="/pure-min.css">
     <link rel="stylesheet" href="/grids-responsive-min.css">
